e4f61a5267b2672f31d3f95549df3cc63b0b26af
Nur Mohammad <nurmohammad6465@gmail.com>
2026-09-14 00:10:23 +0600

    Nested Routing configuration & Outlet

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,27 @@ import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
+import MainLayout from "./layouts/MainLayout";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element:<Home/>,
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      }
+    ],
   },
-  {
-    path: "/about-us",
-    element: <AboutUs/>
-  }
 ]);
 
-
-
 function Router() {
-
   return (
     <>
-       <RouterProvider router={router} />
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default Router
+export default Router;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [click, setClick] = useState("");
+    console.log(click);
     return (
-        <div>
-            This is home page
+        <div className = "text-center">
+            <h1 className="text-4xl text-blue-500 font-bold">
+                <span className="text-amber-300">Weather</span> App</h1>
+            <p className="py-3 text-md text-green-400">
+                Check your weather today in Weather App</p>   
+
+            <div>
+            <button type="button" 
+            onClick={() => setClick("Button clicked!")}
+            className="bg-blue-500 text-white font-bold py-2 px-4 rounded-2xl hover:scale-105 transition-all delay-300">
+                Check Weather</button>
+            </div>
         </div>
     );
 };
